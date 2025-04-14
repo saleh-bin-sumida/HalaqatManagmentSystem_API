34d57be6e7630bac4dca374eb7c00cec47505f5d
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-04-14 14:23:36 +0300

    Add HalaqaProfile class for entity-DTO mapping
    Introduced a new static class `HalaqaProfile` in the `HalaqatSchoolSystem.Core.Mapping` namespace. This class includes methods for mapping `Halaqa` entities to DTOs, specifically:
    
    - `ToGetHalaqaNameDto`: Maps `Halaqa` to `GetHalaqaNameDto`.
    - `ToGetHalaqaDto`: Maps `Halaqa` to `GetHalaqaDto` with additional properties.
    - `ToHalaqa`: Converts `AddHalaqaDto` to `Halaqa`.
    - `UpdateHalaqa`: Updates the `Name` property of a `Halaqa` object.

---
+++ Mapping/HalaqaProfile.cs
@@ -0,0 +1,40 @@
+﻿namespace HalaqatSchoolSystem.Core.Mapping;
+
+public static class HalaqaProfile
+{
+    public static Expression<Func<Halaqa, GetHalaqaNameDto>> ToGetHalaqaNameDto()
+    {
+        return halaqa => new GetHalaqaNameDto
+        {
+            Id = halaqa.Id,
+            Name = halaqa.Name,
+        };
+    }
+
+    public static Expression<Func<Halaqa, GetHalaqaDto>> ToGetHalaqaDto()
+    {
+        return halaqa => new GetHalaqaDto
+        {
+            Id = halaqa.Id,
+            Name = halaqa.Name,
+            TeacherName = halaqa.Teacher.FirstName + " " + halaqa.Teacher.LastName,
+            TeacherId = halaqa.TeacherId,
+            TotalStudents = halaqa.Students.Count
+        };
+    }
+
+    public static Halaqa ToHalaqa(this AddHalaqaDto addHalaqaDto)
+    {
+        return new Halaqa
+        {
+            Name = addHalaqaDto.Name,
+            TeacherId = addHalaqaDto.TeacherId,
+        };
+    }
+
+    public static void UpdateHalaqa(this Halaqa halaqa, UpdateHalaqaDto updateHalaqaDto)
+    {
+        halaqa.Name = updateHalaqaDto.Name;
+
+    }
+}
