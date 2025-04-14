9f40b51f8d2d5290c9aca36e8db0ca6a585c5dda
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-04-14 14:21:54 +0300

    Add DTOs and Halaqa entity for management system
    This commit introduces new Data Transfer Objects (DTOs) in `HalaqaDTOs.cs`, including `GetHalaqaNameDto`, `GetHalaqaDto`, `AddHalaqaDto`, and `UpdateHalaqaDto`, to facilitate data transfer for Halaqa operations.
    
    Additionally, a new `Halaqa` class is defined in `Halaqa.cs`, encompassing properties such as `Id`, `Name`, `TeacherId`, and collections for `Students`, `EnrollmentsHistories`, `TeachingAssignments`, and `StudentAttendances`, enhancing the representation of Halaqa entities.

---
+++ Dtos/HalaqaDTOs.cs
@@ -0,0 +1,34 @@
+﻿namespace HalaqatManagmentSystem_API.Dtos;
+
+
+public record class GetHalaqaNameDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+}
+
+public record class GetHalaqaDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? TeacherName { get; set; }
+    public Guid? TeacherId { get; set; }
+    public int? TotalStudents { get; set; }
+}
+
+
+
+
+
+public class AddHalaqaDto
+{
+    public string Name { get; set; }
+    public Guid TeacherId { get; set; }
+}
+
+public class UpdateHalaqaDto
+{
+    public string Name { get; set; }
+
+}
---
+++ Entities/Halaqa.cs
@@ -0,0 +1,16 @@
+﻿
+namespace HalaqatManagmentSystem_API.Entities;
+
+public class Halaqa
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public Guid? TeacherId { get; set; }
+    public Teacher? Teacher { get; set; }
+
+    public ICollection<Student>? Students { get; set; }
+    public ICollection<StudentHalaqaEnrollment> EnrollmentsHistories { get; set; }
+    public ICollection<TeachingAssignment> TeachingAssignments { get; set; }
+    public ICollection<StudentAttendance> StudentAttendances { get; set; }
+
+}
