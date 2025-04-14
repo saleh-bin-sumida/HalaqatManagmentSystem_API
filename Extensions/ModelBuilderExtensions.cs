085d59fe6bc63886eb5343132dd16911330d71f4
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-04-14 14:32:32 +0300

    Add extension methods for expressions and model constraints
    This commit introduces two new extension classes:
    `ExpressionExtensions` with the `AndAlso` method for combining
    expressions using a logical AND, and `ModelBuilderExtensions`
    with the `AddEnumCheckConstraint` method to enforce check
    constraints on enum values in the database.

---
+++ Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HalaqatManagmentSystem_API.Extensions
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void AddEnumCheckConstraint<TEnum>(this EntityTypeBuilder builder, string tableName, string columnName) where TEnum : Enum
+        {
+            var enumValues = Enum.GetValues(typeof(TEnum)).Cast<int>().ToList();
+            var isLinear = enumValues.Zip(enumValues.Skip(1), (a, b) => b - a).All(diff => diff == 1);
+
+            if (isLinear)
+            {
+                var minValue = enumValues.Min();
+                var maxValue = enumValues.Max();
+
+                builder.ToTable(tableName, t =>
+                {
+                    t.HasCheckConstraint($"CK_{tableName}_{columnName}", $"[{columnName}] >= {minValue} AND [{columnName}] <= {maxValue}");
+                });
+            }
+        }
+    }
+}
