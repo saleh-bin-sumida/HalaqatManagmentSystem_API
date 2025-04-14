a37a1b25e4596ea0f0d73c5cfcc7d2a2c16aeef0
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-04-14 14:22:56 +0300

    Add DTOs for Student Enrollment and Teaching Assignment
    Introduced two new record classes: `StudentEnrollmentDto` and `TeachingAssingmentDto` in the `HalaqatManagmentSystem_API.Dtos` namespace.
    
    `StudentEnrollmentDto` manages student enrollment details with properties like `Id`, `StudentId`, `HalaqaId`, `FullName`, `EnrolledToHalaqa`, `EnrollmentDate`, `EndDate`, and `IsCurrent`.
    
    `TeachingAssingmentDto` handles teaching assignments with properties including `Id`, `TeacherId`, `TeacherFirstName`, `Halaqa_Id`, `HalaqaName`, `StartDate`, `EndDate`, and `IsCurrent`. Both classes support nullable types for optional values.

---
+++ Dtos/StudentEnrollmentDto.cs
@@ -0,0 +1,14 @@
+﻿namespace HalaqatManagmentSystem_API.Dtos;
+
+public record class StudentEnrollmentDto
+{
+    public int Id { get; set; }
+    public Guid StudentId { get; set; }
+    public int HalaqaId { get; set; }
+    public string? FullName { get; set; }
+    public string? EnrolledToHalaqa { get; set; }
+    public DateTime EnrollmentDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public bool IsCurrent { get; set; }
+
+}
---
+++ Dtos/TeachingAssingmentDto.cs
@@ -0,0 +1,14 @@
+﻿namespace HalaqatManagmentSystem_API.Dtos;
+
+
+public record class TeachingAssingmentDto
+{
+    public int Id { get; set; }
+    public Guid TeacherId { get; set; }
+    public string? TeacherFirstName { get; set; }
+    public int Halaqa_Id { get; set; }
+    public string? HalaqaName { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public bool IsCurrent { get; set; }
+}
