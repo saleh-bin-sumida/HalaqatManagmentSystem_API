1f1a91afb79bfb1e869f8040f85884cd9fdfdbcf
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-04-14 14:22:46 +0300

    Add StudentHalaqaEnrollment and TeachingAssignment classes
    Introduced two new classes in the HalaqatManagmentSystem_API.Entities namespace:
    `StudentHalaqaEnrollment` and `TeachingAssignment`. These classes include properties
    for managing student enrollments and teaching assignments, with relationships to
    `Student`, `Teacher`, and `Halaqa`, as well as date tracking and a status indicator.

---
+++ Entities/StudentHalaqaEnrollment.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace HalaqatManagmentSystem_API.Entities;
+
+
+public class StudentHalaqaEnrollment
+{
+    public int Id { get; set; }
+    public Guid StudentId { get; set; }
+    public Student Student { get; set; }
+
+    public int HalaqaId { get; set; }
+    public Halaqa Halaqa { get; set; }
+
+    public DateTime StartDate { get; set; } = DateTime.Now;
+    public DateTime? EndDate { get; set; }
+    public bool IsCurrent { get; set; }
+}
---
+++ Entities/TeachingAssignment.cs
@@ -0,0 +1,14 @@
+﻿namespace HalaqatManagmentSystem_API.Entities;
+
+public class TeachingAssignment
+{
+    public int Id { get; set; }
+    public Guid TeacherId { get; set; }
+    public Teacher Teacher { get; set; }
+    public int HalaqaId { get; set; }
+    public Halaqa Halaqa { get; set; }
+    public DateTime StartDate { get; set; } = DateTime.Now;
+    public DateTime? EndDate { get; set; }
+    public bool IsCurrent { get; set; }
+
+}
