d54b28f94b55ceb84398e4b1acadeb79ea283af5
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-04-14 14:24:14 +0300

    Add mapping profiles for student enrollment and teaching assignment
    This commit introduces two new static mapping classes:
    `StudentEnrollmentProfile` and `TeachingAssignmentProfile` in
    the `HalaqatSchoolSystem.Core.Mapping` namespace. These classes
    provide methods to convert between domain models
    (`StudentHalaqaEnrollment` and `TeachingAssignment`) and their
    corresponding Data Transfer Objects (DTOs)
    (`StudentEnrollmentDto` and `TeachingAssingmentDto`). The
    methods facilitate the creation and conversion of DTOs,
    enhancing data handling capabilities within the application.

---
+++ Mapping/StudentEnrollmentProfile.cs
@@ -0,0 +1,34 @@
+﻿namespace HalaqatSchoolSystem.Core.Mapping
+{
+    public static class StudentEnrollmentProfile
+    {
+        public static StudentEnrollmentDto ToStudentEnrollmentDto(this StudentHalaqaEnrollment studentEnrollment)
+        {
+            return new StudentEnrollmentDto
+            {
+                Id = studentEnrollment.Id,
+                StudentId = studentEnrollment.StudentId,
+                HalaqaId = studentEnrollment.HalaqaId,
+                FullName = studentEnrollment.Student.FirstName + " " + studentEnrollment.Student.LastName,
+                EnrolledToHalaqa = studentEnrollment.Halaqa.Name,
+                EnrollmentDate = studentEnrollment.StartDate,
+                EndDate = studentEnrollment.EndDate,
+                IsCurrent = studentEnrollment.IsCurrent
+            };
+        }
+
+        public static StudentHalaqaEnrollment ToStudentEnrollment(this StudentEnrollmentDto studentEnrollmentDto)
+        {
+            return new StudentHalaqaEnrollment
+            {
+                Id = studentEnrollmentDto.Id,
+                StudentId = studentEnrollmentDto.StudentId,
+                HalaqaId = studentEnrollmentDto.HalaqaId,
+                StartDate = studentEnrollmentDto.EnrollmentDate,
+                EndDate = studentEnrollmentDto.EndDate,
+                IsCurrent = studentEnrollmentDto.IsCurrent
+            };
+        }
+    }
+}
+
---
+++ Mapping/TeachingAssignmentProfile.cs
@@ -0,0 +1,35 @@
+﻿namespace HalaqatSchoolSystem.Core.Mapping
+{
+    public static class TeachingAssignmentProfile
+    {
+        public static TeachingAssingmentDto ToTeachingAssingmentDto(this TeachingAssignment teachingAssignment)
+        {
+            return new TeachingAssingmentDto
+            {
+                Id = teachingAssignment.Id,
+                TeacherId = teachingAssignment.TeacherId,
+                TeacherFirstName = teachingAssignment.Teacher.FirstName,
+                Halaqa_Id = teachingAssignment.HalaqaId,
+                HalaqaName = teachingAssignment.Halaqa.Name,
+                StartDate = teachingAssignment.StartDate,
+                EndDate = teachingAssignment.EndDate,
+                IsCurrent = teachingAssignment.IsCurrent
+            };
+        }
+
+        public static TeachingAssignment ToTeachingAssignment(this TeachingAssingmentDto teachingAssingmentDto)
+        {
+            return new TeachingAssignment
+            {
+                Id = teachingAssingmentDto.Id,
+                TeacherId = teachingAssingmentDto.TeacherId,
+                HalaqaId = teachingAssingmentDto.Halaqa_Id,
+                StartDate = teachingAssingmentDto.StartDate,
+                EndDate = teachingAssingmentDto.EndDate,
+                IsCurrent = teachingAssingmentDto.IsCurrent
+            };
+        }
+    }
+}
+
+
