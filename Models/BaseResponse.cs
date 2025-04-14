b8d62f98aef7376433213909517d0c57bb92bb3d
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-04-14 14:31:52 +0300

    Add BaseResponse and PagedResult classes
    Implemented BaseResponse<T> for standardized API responses with serialization support. Added PagedResult<T> for handling paginated data, including properties for pagination status. Removed commented-out code for clarity.

---
+++ Models/BaseResponse.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace HalaqatManagmentSystem_API.Models;
+
+public class BaseResponse<T>
+{
+    public bool Success { get; set; }
+    public string Message { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public List<string>? Errors { get; set; }
+
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+    public T? Data { get; set; }
+
+    //public BaseResponse(bool success, string message, T? data, List<string>? errors = null)
+    //{
+    //    Data = data;
+    //    Message = message;
+    //    Errors = errors;
+    //    Success = success;
+    //}
+
+    public override string ToString()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+}
---
+++ Models/PagedResult.cs
@@ -0,0 +1,35 @@
+﻿//using Microsoft.EntityFrameworkCore;
+
+namespace HalaqatManagmentSystem_API.Models;
+
+public class PagedResult<T>
+{
+    public required List<T> Items { get; set; }
+
+    public int CurrentPage { get; set; }
+    public int TotalPages { get; set; }
+    public int PageSize { get; set; }
+    public int TotalItems { get; set; }
+    public bool HasNextPage => PageSize * CurrentPage < TotalItems;
+    public bool HasPreveiosPage => CurrentPage > 1;
+
+    //private PagedResult()
+    //{
+
+    //}
+
+    //public static async Task<PagedResult<T>> ToPagedResult(IQueryable<T> query, int pageSize, int pageNumber)
+    //{
+    //    var totalItems = await query.CountAsync();
+    //    var items = await query.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+    //    return new PagedResult<T>
+    //    {
+    //        Items = items,
+    //        TotalItems = totalItems,
+    //        PageSize = pageSize,
+    //        CurrentPage = pageNumber,
+    //        TotalPages = (int)Math.Ceiling(totalItems / (double)pageSize)
+    //    };
+    //}
+}
