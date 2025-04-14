2da88ceaaa286cb82ed7953057097fec8c78984e
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-04-14 14:22:35 +0300

    Add DTOs and StudentAttendance model for attendance data
    Introduces new Data Transfer Objects (DTOs) in the
    `StudentAttendanceDTOs.cs` file, including `GetAttendanceDto`,
    `GetStudentAttendanceDto`, and `AddUpdatedAttendanceDto` for
    facilitating attendance-related data transfer.
    
    Also adds the `StudentAttendance` class in `StudentAttendance.cs`,
    which models student attendance records with relevant properties.

---
+++ Dtos/StudentAttendanceDTOs.cs
@@ -0,0 +1,43 @@
+﻿namespace HalaqatManagmentSystem_API.Dtos;
+
+
+public record class GetAttendanceDto
+{
+    public int? AttendanceId { get; set; }
+    public Guid StudentId { get; set; }
+    public string FullName { get; set; }
+    public int HalaqaId { get; set; }
+    //public DateOnly AttendanceDate { get; set; }
+    public AttendanceStatus? Status { get; set; }
+    public string? Remarks { get; set; }
+
+}
+
+public record class GetStudentAttendanceDto
+{
+    public Guid Id { get; set; }
+    public string FullName { get; set; }
+    public AttendanceStatus AttendanceStatus { get; set; }
+    public int HalaqaId { get; set; }
+}
+
+
+
+public record AddUpdatedAttendanceDto(
+    Guid StudentId,
+    int HalaqaId,
+    DateOnly AttendanceDate,
+    [Range(0,4)]
+    AttendanceStatus Status,
+    string? Remarks);
+
+
+//public record UpdateAttendanceDto(
+//    //Guid? StudentId,
+//    //Student? Student,
+//    //int? Halaqa_Id,
+//    //Halaqa? Halaqa,
+//    //DateTime AttendanceDate,
+//    [Range(0,4)]
+//    AttendanceStatus Status,
+//    string? Remarks);
---
+++ Entities/StudentAttendance.cs
@@ -0,0 +1,15 @@
+﻿namespace HalaqatManagmentSystem_API.Entities;
+
+public class StudentAttendance
+{
+    public int Id { get; set; }
+    public Guid StudentId { get; set; }
+    public Student Student { get; set; }
+    public int HalaqaId { get; set; }
+    public Halaqa Halaqa { get; set; }
+    public DateOnly AttendanceDate { get; set; }
+    public AttendanceStatus Status { get; set; }
+    public string? Remarks { get; set; }
+}
+
+
