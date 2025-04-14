f972ad8ce72ac10da242710f041ee96581928559
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-04-14 14:22:17 +0300

    Add DTOs and Student class for data management
    Introduced new Data Transfer Objects (DTOs) in `StudentDTOs.cs` for handling student data retrieval and updates, including `GetStudentNameDto`, `GetStudentDto`, `GetStudentFullInfoDto`, `AddStudentDto`, and `UpdateStudentDto`.
    
    Defined a new `Student` class in `Student.cs` with comprehensive properties for student details, including personal information, relationships, and collections for enrollment histories and attendance records.

---
+++ Dtos/StudentDTOs.cs
@@ -0,0 +1,93 @@
+﻿
+namespace HalaqatManagmentSystem_API.Dtos;
+
+
+public record class GetStudentNameDto
+{
+    public Guid Id { get; set; }
+    public string FullName { get; set; }
+
+}
+
+
+
+
+public record class GetStudentDto
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public bool Gender { get; set; }
+
+    public int Age { get; set; }
+
+    public string? HalaqaName { get; set; }
+
+    public SchoolGrade SchoolGrade { get; set; }
+    public int? HalaqaId { get; set; }
+}
+
+public class GetStudentFullInfoDto
+{
+    public Guid Id { get; set; }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int SerialNumber { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public bool Gender { get; set; }
+
+    public string? PhoneNumber { get; set; }
+    public string? City { get; set; }
+    public string? Neighborhood { get; set; }
+
+    public Guid? GuardianId { get; set; }
+    public string GuardianFullName { get; set; }
+    public string GuardianPhoneNumber { get; set; }
+
+    public GuardianKinship? GuardianKinship { get; set; }
+    public int? HalaqaId { get; set; }
+    public string? HalaqaName { get; set; }
+    public Surah LastMemorizedSurah { get; set; }
+    public SchoolGrade SchoolGrade { get; set; }
+}
+
+
+public class AddStudentDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+    public bool Gender { get; set; } = true;
+    [Phone]
+    public string? PhoneNumber { get; set; }
+    public string Neighborhood { get; set; }
+    public string? City { get; set; }
+
+    public Guid GuardianId { get; set; }
+    public GuardianKinship GuardianKinship { get; set; }
+    //public int? HalaqaId { get; set; }
+    public Surah LastMemorizedSurah { get; set; }
+    public SchoolGrade SchoolGrade { get; set; }
+}
+
+public class UpdateStudentDto
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+    public bool Gender { get; set; } = true;
+    [Phone]
+
+    public string? PhoneNumber { get; set; }
+    public string Neighborhood { get; set; }
+    public string? City { get; set; }
+
+    public Guid? GuardianId { get; set; }
+    public GuardianKinship? GuardianKinship { get; set; }
+    public Surah LastMemorizedSurah { get; set; }
+    public SchoolGrade SchoolGrade { get; set; }
+}
+
---
+++ Entities/Student.cs
@@ -0,0 +1,30 @@
+﻿
+namespace HalaqatManagmentSystem_API.Entities;
+
+public class Student
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public bool Gender { get; set; } = true;
+    public DateTime DateOfBirth { get; set; }
+    public string PhoneNumber { get; set; }
+
+    public string? Neighborhood { get; set; }
+    public string? City { get; set; }
+    public Surah LastMemorizedSurah { get; set; }
+    public QuranicNarration QuranicNarration { get; set; }
+    public SchoolGrade SchoolGrade { get; set; }
+    public int SerialNumber { get; set; }
+
+    public int? HalaqaId { get; set; }
+    public Halaqa? Halaqa { get; set; }
+    public Guid? GuardianId { get; set; }
+    public Guardian? Guardian { get; set; }
+    public GuardianKinship? GuardianKinship { get; set; }
+
+    public ICollection<StudentHalaqaEnrollment> EnrollmentsHistories { get; set; } = new List<StudentHalaqaEnrollment>();
+    public ICollection<StudentAttendance> StudentAttendances { get; set; } = new List<StudentAttendance>();
+
+}
+
