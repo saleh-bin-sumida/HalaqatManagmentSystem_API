7c0862b949324bf1f1c03399900bcfc441d6d495
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-04-14 14:25:57 +0300

    Add AttendanceValueAttribute and SurahInfoAttribute classes
    Introduced the `AttendanceValueAttribute` class to represent attendance values with a double property. Added the `SurahInfoAttribute` class to encapsulate information about a Surah, including its name and total verses. Both classes are defined in the `HalaqatSchoolSystem.Shared.Attributes` namespace and are marked with appropriate attribute usage.

---
+++ Attributes/AttendanceValueAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace HalaqatSchoolSystem.Shared.Attributes;
+
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+public class AttendanceValueAttribute(double value) : Attribute
+{
+    public double Value { get; } = value;
+
+}
---
+++ Attributes/SurahInfoAttribute.cs
@@ -0,0 +1,16 @@
+﻿
+namespace HalaqatSchoolSystem.Shared.Attributes;
+
+
+[AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+sealed class SurahInfoAttribute : Attribute
+{
+    public string Name { get; }
+    public int TotalVerses { get; }
+
+    public SurahInfoAttribute(string name, int totalVerses)
+    {
+        Name = name;
+        TotalVerses = totalVerses;
+    }
+}
