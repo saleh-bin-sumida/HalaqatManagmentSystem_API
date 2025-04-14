77168071e13cafac2c6f9f3a69568df00669249b
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-04-14 14:23:12 +0300

    Add GuardianProfile class for DTO mapping
    Introduces the `GuardianProfile` static class in the `HalaqatSchoolSystem.Core.Mapping` namespace. This class provides methods to map `Guardian` entities to `GetGuardianNameDto`, `GetGuardianDto`, and `GetGuardianFullInfoDto`. It also includes extension methods for converting `AddGuardianDto` to `Guardian` and updating a `Guardian` with `UpdateGuardianDto` data. Enhancements include support for additional properties: `Occupation`, `IdentityType`, `IdentityNumber`, and `MaritalStatus`.

---
+++ Mapping/GuardianProfile.cs
@@ -0,0 +1,77 @@
+﻿namespace HalaqatSchoolSystem.Core.Mapping;
+
+public static class GuardianProfile
+{
+    public static Expression<Func<Guardian, GetGuardianNameDto>> ToGetGuardianNameDto()
+    {
+        return guardian => new GetGuardianNameDto
+        {
+            Id = guardian.Id,
+            FullName = guardian.FirstName + " " + guardian.LastName
+        };
+    }
+
+    public static Expression<Func<Guardian, GetGuardianDto>> ToGetGuardianDto()
+    {
+        return guardian => new GetGuardianDto
+        {
+            Id = guardian.Id,
+            FirstName = guardian.FirstName,
+            LastName = guardian.LastName,
+            Gender = guardian.Gender,
+            Occupation = guardian.Occupation,
+            PhoneNumber = guardian.PhoneNumber
+        };
+    }
+
+    public static Expression<Func<Guardian, GetGuardianFullInfoDto>> ToGetGuardianFullInfoDto()
+    {
+        return guardian => new GetGuardianFullInfoDto
+        {
+            Id = guardian.Id,
+            FirstName = guardian.FirstName,
+            LastName = guardian.LastName,
+            Gender = guardian.Gender,
+            PhoneNumber = guardian.PhoneNumber,
+
+            Occupation = guardian.Occupation,
+
+            IdentityType = guardian.IdentityType,
+            IdentityNumber = guardian.IdentityNumber,
+            MaritalStatus = guardian.MaritalStatus,
+        };
+    }
+
+    public static Guardian ToGuardian(this AddGuardianDto addGuardianDto)
+    {
+        var guardian = new Guardian
+        {
+            FirstName = addGuardianDto.FirstName,
+            LastName = addGuardianDto.LastName,
+            Gender = addGuardianDto.Gender,
+            PhoneNumber = addGuardianDto.PhoneNumber,
+
+            Occupation = addGuardianDto.Occupation,
+
+            IdentityType = addGuardianDto.IdentityType,
+            IdentityNumber = addGuardianDto.IdentityNumber,
+            MaritalStatus = addGuardianDto.MaritalStatus,
+        };
+
+        return guardian;
+    }
+
+    public static void UpdateGuardian(this Guardian guardian, UpdateGuardianDto updateGuardianDto)
+    {
+        guardian.FirstName = updateGuardianDto.FirstName;
+        guardian.LastName = updateGuardianDto.LastName;
+        guardian.Gender = updateGuardianDto.Gender;
+        guardian.PhoneNumber = updateGuardianDto.PhoneNumber;
+
+        guardian.Occupation = updateGuardianDto.Occupation;
+
+        guardian.IdentityType = updateGuardianDto.IdentityType;
+        guardian.IdentityNumber = updateGuardianDto.IdentityNumber;
+        guardian.MaritalStatus = updateGuardianDto.MaritalStatus;
+    }
+}
