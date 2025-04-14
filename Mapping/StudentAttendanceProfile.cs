9fd8e1532dcf2092da6a4acd6513e164af7c7ec2
Saleh Mohammed <salhbnsmyd3@gmail.com>
2025-04-14 14:23:48 +0300

    Add StudentAttendanceProfile with extension methods
    Introduced a new static class `StudentAttendanceProfile` in the `HalaqatSchoolSystem.Core.Mapping` namespace. This class features three extension methods:
    - `ToGetAttendanceDto` for converting `StudentAttendance` to `GetAttendanceDto`.
    - `ToStudentAttendance` for converting `AddUpdatedAttendanceDto` to `StudentAttendance`.
    - `UpdateStudentAttendance` for updating the `Status` and `Remarks` of a `StudentAttendance` object.

---
+++ Mapping/StudentAttendanceProfile.cs
@@ -0,0 +1,38 @@
+﻿namespace HalaqatSchoolSystem.Core.Mapping
+{
+    public static class StudentAttendanceProfile
+    {
+        public static GetAttendanceDto ToGetAttendanceDto(this StudentAttendance studentAttendance)
+        {
+            return new GetAttendanceDto
+            {
+                AttendanceId = studentAttendance.Id,
+                StudentId = studentAttendance.StudentId,
+                FullName = studentAttendance.Student.FirstName + " " + studentAttendance.Student.LastName,
+                HalaqaId = studentAttendance.HalaqaId,
+                Status = studentAttendance.Status,
+                Remarks = studentAttendance.Remarks
+            };
+        }
+
+
+        public static StudentAttendance ToStudentAttendance(this AddUpdatedAttendanceDto addAttendanceDto)
+        {
+            return new StudentAttendance
+            {
+                StudentId = addAttendanceDto.StudentId,
+                HalaqaId = addAttendanceDto.HalaqaId,
+                AttendanceDate = addAttendanceDto.AttendanceDate,
+                Status = addAttendanceDto.Status,
+                Remarks = addAttendanceDto.Remarks
+            };
+        }
+
+        public static void UpdateStudentAttendance(this StudentAttendance studentAttendance, AddUpdatedAttendanceDto addUpdateAttendanceDto)
+        {
+            studentAttendance.Status = addUpdateAttendanceDto.Status;
+            studentAttendance.Remarks = addUpdateAttendanceDto.Remarks;
+        }
+    }
+}
+
